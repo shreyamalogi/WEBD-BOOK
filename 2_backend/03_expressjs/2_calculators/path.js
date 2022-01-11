4bbfa74383207ae97dc0a492b90e1f4bc35abcf5
codemacrocosm <5hre9a@gmail.com>
2022-01-12 00:13:17 +0530

    initial

---
+++ 2_backend/03_expressjs/2_calculators/path.js
@@ -0,0 +1,9 @@
+//if the __dirname is at all confusing for u
+// we gonna create Path.js and console log it
+//then run by using node path.js we get the location
+// C:\Users\Shre9\Calculator-app>node path.js
+// C:\Users\Shre9\Calculator-app
+
+
+
+console.log(__dirname);
